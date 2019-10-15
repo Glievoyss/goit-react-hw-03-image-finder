e7c5ec56afa8b043c0ed49b92828e98dbd1bdea6
Glievoyss <glievoy.s@gmail.com>
2019-10-15 17:31:33 +0300

    Updates

---
+++ precache-manifest.52ff359dfeb2814b57dfff75ad913ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d816b844fe7ac762619e3013709e2ffb",
+    "url": "/goit-react-hw-03-image-finder/./index.html"
+  },
+  {
+    "revision": "31ad95f1134400ef9a6d",
+    "url": "/goit-react-hw-03-image-finder/./static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "f21b42cece4df74688f6",
+    "url": "/goit-react-hw-03-image-finder/./static/css/main.7835dc88.chunk.css"
+  },
+  {
+    "revision": "31ad95f1134400ef9a6d",
+    "url": "/goit-react-hw-03-image-finder/./static/js/2.2e28ddcd.chunk.js"
+  },
+  {
+    "revision": "f21b42cece4df74688f6",
+    "url": "/goit-react-hw-03-image-finder/./static/js/main.b973a4bd.chunk.js"
+  },
+  {
+    "revision": "12f293b10c7160f06dd6",
+    "url": "/goit-react-hw-03-image-finder/./static/js/runtime-main.18163538.js"
+  }
+]);
